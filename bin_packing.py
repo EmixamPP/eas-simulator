2c647f51cf3c31424d8f53b993c753b4673daccf
EmixamPP <dev@emixam.be>
2023-05-13 16:09:51 +0200

    experiment bin packing nextfit under condition

--- bin_packing.py
+++ bin_packing.py
@@ -47,44 +47,82 @@ class NextfitCond(Placer):
         bin.place(item)
 
 
+class Nextfit(Placer):
+    def __init__(self, nbr_bin: int) -> None:
+        super().__init__(nbr_bin)
+        self.prev_bin_i: int = 0
+
+    def place(self, item: float) -> None:
+        bin_i: int = (self.prev_bin_i + 1) % len(self.bins)
+        bin = self.bins[bin_i]
+        self.total_step += 1
+
+        self.prev_bin_i = bin_i
+        bin.place(item)
+
+
+def compute_boxplot(data: np.ndarray):
+    median = np.percentile(data, 50)
+    q1 = np.percentile(data, 25)
+    q3 = np.percentile(data, 75)
+    iqr = q3 - q1
+    whisker_min = data[data >=
+                       q1 - 1.5 * iqr].min()
+    whisker_max = data[data <=
+                       q3 + 1.5 * iqr].max()
+    outers = data[(data < whisker_min) | (
+        data > whisker_max)]
+    return (median.round(2), q1.round(2), q3.round(2), whisker_min.round(2), whisker_max.round(2), outers.round(2))
+
+
 def run_experiment_with(nbr_bin: int):
-    with open(f"diff_nextfitcond_worstfit_{nbr_bin}bins.csv", "w") as file_diff,\
-            open(f"steps_nextfitcond_worstfit_{nbr_bin}bins.csv", "w") as file_steps:
-        for total_val in [20, 60, 100]:
-            for nbr_item in [nbr_bin*2, nbr_bin*4]:
+    with open(f"diff_std_binpacking_{nbr_bin}bins.csv", "w") as file:
+        file.write("items, median, q1, q3, whisker_min, whisker_max, outers\n")
 
-                std_diff_hist = []
-                step_hist = []
-                for repetition in range(10000):
+        for nbr_item in [nbr_bin * 2, nbr_bin * 4, nbr_bin * 8]:
 
-                    gen: npr.Generator = npr.Generator(npr.PCG64(repetition))
-                    worstfit: Placer = Worstfit(nbr_bin)
-                    nextfit: Placer = NextfitCond(nbr_bin)
+            nextfitcond_std_diff_hist = []
+            nextfit_std_diff_hist = []
+            nextfitcond_step_diff_hist = []
+            for repetition in range(10000):
 
-                    items = gen.random(nbr_item)
-                    items *= (nbr_bin * total_val) / np.sum(items)
+                worstfit: Placer = Worstfit(nbr_bin)
+                nextfitcond: Placer = NextfitCond(nbr_bin)
+                nextfit: Placer = Nextfit(nbr_bin)
 
-                    for item in items:
-                        worstfit.place(item)
-                        nextfit.place(item)
+                gen: npr.Generator = npr.Generator(npr.PCG64(repetition))
+                items = gen.random(nbr_item)
+                for item in items:
+                    worstfit.place(item)
+                    nextfitcond.place(item)
+                    nextfit.place(item)
 
-                    nextfit_loads = np.array(
-                        [nextfit.bins[i].cap for i in range(nbr_bin)])
-                    nextfit_std = nextfit_loads.std()
-                    worstfit_loads = np.array(
-                        [worstfit.bins[i].cap  for i in range(nbr_bin)])
-                    worstfit_std = worstfit_loads.std()
-                    std_diff = worstfit_std / nextfit_std * 100
+                worstfit_std = np.array(
+                    [worstfit.bins[i].cap for i in range(nbr_bin)]).std()
+                nextfitcond_std = np.array(
+                    [nextfitcond.bins[i].cap for i in range(nbr_bin)]).std()
+                nextfit_std = np.array(
+                    [nextfit.bins[i].cap for i in range(nbr_bin)]).std()
 
-                    std_diff_hist.append(std_diff)
-                    step_hist.append(nextfit.total_step)
+                nextfitcond_std_diff_hist.append(worstfit_std - nextfitcond_std)
+                nextfit_std_diff_hist.append(worstfit_std - nextfit_std)
+                nextfitcond_step_diff_hist.append(
+                    worstfit.total_step - nextfitcond.total_step)
 
-                file_diff.write("{}, {}\n".format(
-                    f"load{total_val}_items{nbr_item}",
-                    str(std_diff_hist)[1:-1]))
-                file_steps.write("{}, {}\n".format(
-                    f"load{total_val}_items{nbr_item}",
-                    str(step_hist)[1:-1]))
+            median, q1, q3, whisker_min, whisker_max, outers = compute_boxplot(
+                np.array(nextfitcond_std_diff_hist))
+            file.write(
+                f"nextfitcond_std_items{nbr_item}, {median}, {q1}, {q3}, {whisker_min}, {whisker_max}, {tuple(outers)}\n")
+
+            median, q1, q3, whisker_min, whisker_max, outers = compute_boxplot(
+                np.array(nextfitcond_step_diff_hist))
+            file.write(
+                f"nextfitcond_step_items{nbr_item}, {median}, {q1}, {q3}, {whisker_min}, {whisker_max}, {tuple(outers)}\n")
+
+            median, q1, q3, whisker_min, whisker_max, outers = compute_boxplot(
+                np.array(nextfit_std_diff_hist))
+            file.write(
+                f"nextfit_std_items{nbr_item}, {median}, {q1}, {q3}, {whisker_min}, {whisker_max}, {tuple(outers)}\n")
 
 
 if __name__ == "__main__":
